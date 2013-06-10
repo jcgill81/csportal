2d4dc93d6f0928446b143876d37499a09fe1c33b
James Gill <james.gill@moovweb.com>
2013-06-10 16:48:52 -0700

    First commita

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csportal::Application.config.secret_token = 'f0e7b2481f85c812d00b8391f2d5fe227b440ec9819e6a03b5b3cc2359b9ef4302e5cbced05f88b16be6dc5fa9e995686d4afdbc6e6d28f930e9fc9c1a0bda1e'
