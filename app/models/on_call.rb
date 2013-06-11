412005764a4bacaf4630d289560b63bba4be5d9e
James Gill <james.gill@moovweb.com>
2013-06-10 17:43:24 -0700

    Tweaking Model

--- app/models/on_call.rb
+++ app/models/on_call.rb
@@ -1,3 +1,6 @@
 class OnCall < ActiveRecord::Base
   attr_accessible :count, :email, :name, :phone
+  validates :name, :email, :phone, :presence => true
+  validates :name, :email, :phone, :uniqueness => true
+  	:message => "Person Exists" }
 end
