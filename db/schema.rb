d216a868c248d6691a233c36e7d02b003d2d51d0
James Gill <james.gill@moovweb.com>
2013-06-14 09:59:56 -0700

    Trying to implement searchable db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612230441) do
+ActiveRecord::Schema.define(:version => 20130614160359) do
 
   create_table "logins", :force => true do |t|
     t.string   "name"
@@ -22,5 +22,14 @@ ActiveRecord::Schema.define(:version => 20130612230441) do
   end
 
   add_index "logins", ["email"], :name => "index_logins_on_email", :unique => true
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "pm"
+    t.string   "am"
+    t.string   "qa"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
